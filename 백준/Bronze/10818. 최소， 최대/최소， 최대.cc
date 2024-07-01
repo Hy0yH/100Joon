dd3310e232065e037d401f93cf5e3651833e36c8
Hy0yH <imhj1348@gmail.com>
2024-07-01 23:43:48 +0900

    [Bronze III] Title: 최소, 최대, Time: 164 ms, Memory: 5016 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -1,26 +1,16 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <malloc.h>
+
+int CMP[1000000];
 
 int main(void)
 {
-	int N = 0, tmp = 0;
-
+	int N = 0, MIN = 1000000, MAX = -1000000, tmp = 0;
 	scanf("%d\n", &N);
 
-	int* CMP = (int*)malloc(1000000 * sizeof(int));
-	if (CMP == NULL)
-	{
-		fprintf(stderr, "메모리 할당 오류\n");
-		return -1;
-	}
 	for (int i = 0; i < N; i++)
 	{
 		scanf("%d", &CMP[i]);
 	}
-	int MIN = CMP[0];
-	int MAX = CMP[0];
-
 	for (int j = 0; j < N; j++)
 	{
 		tmp = CMP[j];
@@ -31,7 +21,6 @@ int main(void)
 		
 	}
 	printf("%d %d", MIN, MAX);
-	free(CMP);
 
 	return 0;
 }
