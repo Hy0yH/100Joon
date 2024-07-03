eb17238631767894b48f8e5a6a4446fe7fb3abd9
Hy0yH <imhj1348@gmail.com>
2024-07-03 23:58:17 +0900

    [Bronze II] Title: OX퀴즈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+char arr[80];
+
+int main(void)
+{
+	int N = 0, count = 0, tmp = 0;
+	scanf("%d", &N);
+		
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < 80; j++)
+			arr[j] = NULL;
+
+		count = 0;
+		scanf("%s", arr);
+
+
+		for (int k = 0; k < sizeof(arr); k++)
+		{
+			if (arr[k] == 'O')
+				tmp++;
+			else
+				tmp = 0;
+
+			count = count + tmp;
+		}
+		
+		printf("%d\n", count);
+	}
+
+	return 0;
+}
