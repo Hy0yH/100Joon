fadcee014aabe8286bfaefc476f08003b1d27208
Hy0yH <imhj1348@gmail.com>
2024-07-04 21:54:53 +0900

    [Bronze III] Title: ACM 호텔, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
@@ -4,22 +4,13 @@
 int main(void)
 {
 	
-	int T = 0, H = 0, W = 0, N = 0, A = 0, B = 0;
+	int T = 0, H = 0, W = 0, N = 0, A = 0;
 	scanf("%d", &T);
 
 	for (int i = 0; i < T; i++)
 	{
-		int Hotel[99][99] = { 0 };
 		A = 0;
 		scanf("%d %d %d", &H, &W, &N);
-		for (int j = 0; j < H; j++)
-		{
-			for (int k = 0; k < W; k++)
-			{
-				Hotel[j][k] = (j + 1) * 100 + k + 1;
-			}
-		}
-
 		if (N % H == 0)
 		{
 			A = H * 100 + N / H;
