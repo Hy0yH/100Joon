1f7559f713d39ee64c137c719eb2e8e32c7e6324
Hy0yH <imhj1348@gmail.com>
2024-08-22 23:31:18 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n - i; j++)
+		{
+			printf(" ");
+		}
+		for (int k = 1; k <= 2 * i - 1; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (int i = 1; i < n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			printf(" ");
+		}
+		for (int k = 1; k <= 2 * (n - i) - 1; k++)
+		{
+			printf("*");
+		}
+		if (i + 1 == n)
+			continue;
+		printf("\n");
+
+	}
+
+	return 0;
+}
